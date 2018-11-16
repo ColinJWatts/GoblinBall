f1d9733b5e198844284d1485fbde0c5b1adce8e9
Nathan Watts <Nyrt.llammagum@gmail.com>
2018-11-15 21:24:50 -0500

    Added some split-screen camera testing

--- GoblinBall/Assets/CameraManager.cs
+++ GoblinBall/Assets/CameraManager.cs
@@ -1,21 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class CameraManager : MonoBehaviour {
 
-	public Camera[] cams;
+	private List<Camera> cams;
 
-	public Transform[] objs;
+	private List<Transform> objs;
 
-	public int n;
+	private int n = 4;
 
-	private float dist = 1;
 	private float[]  positions= {0,0,0,0};
+
+	public float max_margin = 0.005f;
+	private float dist = 1.666169f;
+
+	[SerializeField] private float ddd = 0f;
 
 	// Use this for initialization
 	void Start () {
-		dist = cams[0].aspect * cams[0].orthographicSize;
+		cams = new List<Camera>();
+		objs = new List<Transform>();
+
+		GameObject[] cam_objs = GameObject.FindGameObjectsWithTag("MainCamera");
+		for (int i = 0; i < cam_objs.Length; i++){
+			cams.Add(cam_objs[i].GetComponent<Camera>());
+		}
+		cams = cams.OrderBy(a=>a.transform.position.x).ToList();
+		
+
+		GameObject[] objs_array = GameObject.FindGameObjectsWithTag("camera_follow");
+		for (int i = 0; i < objs_array.Length; i++){
+			objs.Add(objs_array[i].GetComponent<Transform>());
+		}
+
+
+		n = Mathf.Min(cams.Count, objs.Count);
 	}
 	
 	// Update is called once per frame
@@ -26,31 +47,44 @@ public class CameraManager : MonoBehaviour {
 		System.Array.Sort(positions);
 
 
-		int group_size = 1;
+		int group_n = 1;
 		float group_sum = -100000.0f;
-		for (int i = 0; i < n; i++){
-			if (positions[i] <= group_sum / group_size + dist * (group_size+1)){
-				group_size += 1;
+		for (int i = 0; i < n; i++){  // TODO: groups don't account for cameras moving left
+			if (positions[i] <= group_sum / group_n + dist * (group_n+1)){
+				group_n += 1;
 				group_sum += positions[i];
 			} else {
 				if (i > 0){
-					align_cameras(group_size, group_sum, i); 
+					align_cameras(group_n, group_sum, i); 
 				}
-				group_size = 1;
+				group_n = 1;
 				group_sum = positions[i];
 			}
 		}
-		align_cameras(group_size, group_sum, n);
+		align_cameras(group_n, group_sum, n);
 		
 	}
 
-	// aligns the [group_size] cameras preciding camera[end]
-	private void align_cameras(int group_size, float group_sum, int end){ 
-		for (int j =0; j < group_size; j++){
+	// aligns the [group_n] cameras preciding camera[end]
+	private void align_cameras(int group_n, float group_sum, int end){ 
+		for (int j =0; j < group_n; j++){
 			int i = end - j - 1;
-			positions[i] = group_sum / group_size + dist * (group_size-1) - dist * j * 2;
-			cams[i].transform.position = new Vector3(positions[i], 0,-10);
-			cams[i].rect.x = cams[i].rect.x;
+			positions[i] = group_sum / group_n + dist * (group_n-1) - dist * j * 2;
+			float dist_left = 0;
+			float dist_right = 0;
+			if (i > 0) dist_left = calculate_margin(positions[i], positions[i-1]);
+			if (i < n-1) dist_right = calculate_margin(positions[i+1], positions[i]);
+
+			Rect cam_rect = new Rect(0,0,1,1);
+			cam_rect.xMin = i * 0.25f  + dist_left;
+			cam_rect.xMax = (i+1) * 0.25f - dist_right;
+			cams[i].rect = cam_rect;
+
+			cams[i].transform.position = new Vector3(positions[i] + dist * (dist_left - dist_right) * 4, 0,-10);
 		}
+	}
+
+	private float calculate_margin(float hi, float lo){
+		return (float)Mathf.Max((float)System.Math.Tanh((hi - lo - dist*2) / 20)/50f, 0.0f);
 	}
 }
